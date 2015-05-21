46b2d09518bd7564324dc6439a00bfaf7140dc3e
Julien Sagot <ju.sagot@gmail.com>
2015-05-21 20:36:51 +0200

    Added gallery-admin.php (js and html are missing, actions in php are implemented.

---
+++ src/php/gallery-admin.php
@@ -0,0 +1,44 @@
+<?php
+
+function process ()
+{
+
+  if (! isset ($_POST ['dir']) || ! isset $_POST ['action'])
+    return ;
+
+  $dir = $_POST ['dir']
+
+  $res = [] ;
+
+  switch ( "$_POST ['action']" )
+  {
+    case "add" :
+    $res = (move_uploaded_file ("$_FILES['pic']['tmp_name']",
+                                "$dir/$_FILES['pic']['name']") ) ?
+           [ "add" => "OK" ] :
+           [ "add" => "KO" ] ;
+    break ;
+
+    case "rm" :
+    $res = unlink ("$_POST[path]") ?
+           [ "rm" => "OK" ] :
+           [ "rm" => "KO" ] ;
+    break ;
+
+    case "list" :
+    $res = [ "list" => glob ( "$dir/*.{jpg,JPG,gif,GIF,png,PNG,jpeg,JPEG}",
+                              GLOB_BRACE ) ];
+    break ;
+
+    default:
+    return ;
+
+    echo (json_encode ($res, JSON_FORCE_OBJECT)) ;
+
+    exit ;
+  }
+}
+
+process () ;
+
+?>
